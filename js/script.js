2f75d561f9d7a1823a351630a557fa2160afa57c
Damian Zulco <damez77@gmail.com>
2024-05-08 15:53:50 -0300

    fix suma

--- js/script.js
+++ js/script.js
@@ -1,38 +1,3 @@
-/*1. Escribe un programa de una sola línea que haga que aparezca en la pantalla un
-alert que diga “Hello World”.
-2. Escribe un programa de una sola línea que escriba en la pantalla un texto que
-diga “Hello World” (document.write).
-3. Escribe un programa de una sola línea que escriba en la pantalla el resultado
-de sumar 3 + 5.
-4. Escribe un programa de dos líneas que pida el nombre del user_name con un
-prompt y escriba un texto que diga “Hola nombreuser_name”
-5. Escribe un programa de tres líneas que pida un número, pida otro número y
-escriba el resultado de sumar estos dos números.
-*/
-/*
-alert("Hello World");
-
-document.write("Hola mundo");
-document.write("<br>");
-
-document.write("Suma de 3+5=", 3+5);
-document.write("<br>");
-
-var user_name = prompt("Ingrese nombre de user_name");
-document.write("Hola ",user_name);
-document.write("<br>");
-
-var numberOne = prompt("Ingrese un numero");
-var numberTwo = prompt("Ingrese otro numero");
-document.write("Suma de los nros es:",parseInt(numberOne)+parseInt(numberTwo));
-document.write("<br>");
-*/
-/*Ejercicio extra (combinando .js, .css y .html): Crear una página que
-pida el nombre del user_name, dos valores y nos muestre las 4
-operaciones aritméticas. Todos los datos deberán aparecer en el
-documento, incorporar estilos.
-*/
-
 function inputByPrompt()
 {
     var userName = prompt("Ingrese nombre de usuario");
@@ -54,7 +19,7 @@ function showMathOperations(inputMethod,numberOne,numberTwo,userName)
 {
     var message = "Usuario: "+ userName; 
     message+='\n';
-    message+='Suma de los nros es: '+ parseInt(numberOne)+parseInt(numberTwo);
+    message+='Suma de los nros es: '+ (parseInt(numberOne)+parseInt(numberTwo));
     message+='\n';
     message+='Resta de los nros es: '+ (parseInt(numberOne)-parseInt(numberTwo));
     message+='\n';
@@ -65,19 +30,6 @@ function showMathOperations(inputMethod,numberOne,numberTwo,userName)
     message+='Método de ingreso de datos: '+ inputMethod;
     message+='\n';
     
-    
-    /* 
-    document.write("<br>");
-    document.write("Suma de los nros es:",parseInt(numberOne)+parseInt(numberTwo));
-    document.write("<br>");
-    document.write("Resta de los nros es:",parseInt(numberOne)-parseInt(numberTwo));
-    document.write("<br>");
-    document.write("Multiplicacion de los nros es:",parseInt(numberOne)*parseInt(numberTwo));
-    document.write("<br>");
-    document.write("División de los nros es:",parseInt(numberOne)/parseInt(numberTwo));
-    document.write("<br>");
-    document.write("Método de ingreso de datos: ",inputMethod);
-*/
     document.getElementById('resultados').innerText=message;
 
 }
